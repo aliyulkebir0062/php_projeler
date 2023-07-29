2d65ff6c5ec917857aca21c129bbb00d45f43259
Ali Yülkebir Güneş <104678795+aliyulkebir0062@users.noreply.github.com>
2023-07-29 19:04:35 +0300

    degıskenler.php
    Bu proje, PHP programlama dilinde temel değişkenler ve veri türlerinin kullanımını gösteren basit bir örnek içermektedir. PHP'nin yaygın olarak kullanılan veri türleri olan string, sayı, boolean ve dizi türleri kullanılarak örnekler sunulmuştur.

---
+++ degıskenler.php
@@ -0,0 +1,23 @@
+<?php
+//String türünde bir değişken örneği
+$isim = "Ali Yülkebir";
+echo "Merhaba, benim adım " . $isim . ".<br>";
+
+//Sayı türünde bir değişken örneği
+$yas = *;
+echo "Yaşım: " . $yas . "<br>";
+
+//Boolean türünde bir değişken örneği
+$dogruMu = true;
+echo "Doğru mu? " . ($dogruMu ? "Evet" : "Hayır") . "<br>";
+
+//Dizi (array) türünde bir değişken örneği
+$renkler = array("Kırmızı", "Mavi", "Yeşil");
+echo "Favori renklerim: " . $renkler[0] . ", " . $renkler[1] . ", " . $renkler[2] . "<br>";
+
+//Değişken değerini değiştirme
+$degisken = "Merhaba";
+echo $degisken . "<br>"; //"Merhaba" yazdırır
+$degisken = "Selam";
+echo $degisken . "<br>"; //"Selam" yazdırır
+?>
