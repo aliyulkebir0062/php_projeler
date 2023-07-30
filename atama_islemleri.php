9e6f398aacb5bb9768007a928175185338f2a714
Ali Yülkebir Güneş <104678795+aliyulkebir0062@users.noreply.github.com>
2023-07-30 18:41:53 +0300

    atama_islemleri.php
    Bu kod, PHP'de atama operatörlerini ve arttırma/eksiltme operatörlerini gösteriyor. Atama operatörleri, değişkenin mevcut değerine belirli bir değeri eklemek, çıkarmak, çarpmak veya bölmek için kullanılırken, arttırma ve eksiltme operatörleri, değişkenin değerini birer birer artırmak veya eksiltmek için kullanılır.

---
+++ atama_islemleri.php
@@ -0,0 +1,42 @@
+<?php
+//Atama İşlemleri
+
+$atama=500;
+
+echo "\$atama değişeni:". $atama;
+$atama+=200;
+echo "<hr>";
+echo "\$atama değişkenin değeri :". $atama;
+
+$atama-=200;
+echo "<hr>";
+echo "\$atama değişkenin değeri :". $atama;
+
+$atama*=200;
+echo "<hr>";
+echo "\$atama değişkenin değeri :". $atama;
+
+$atama/=200;
+echo "<hr>";
+echo "\$atama değişkenin değeri :". $atama;
+echo "<hr>";
+
+/*
+arttırma ve eksiltme işlemleri
+*/
+
+$atama=100;
+
+$atama++;
+echo "\$atama değişkeni değeri:". $atama;
+echo "<hr>";
+
+$atama--;
+echo "\$atama değişkeni değeri:". $atama;
+echo "<hr>";
+
+$atama--;
+echo "\$atama değişkeni değeri:". $atama;
+echo "<hr>";
+
+?>
